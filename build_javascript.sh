4523ae5a11a6468816e4a29ae397656fb18758be
Cesar Augusto D. Azevedo <cesardeazevedo@outlook.com>
2014-11-16 23:49:18 -0200

    new structure

--- build_javascript.sh
+++ build_javascript.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 echo "Building javascript"
-cd jsdev
+cd public/jsdev
 
 echo "default theme"
 sh build_release.sh en_US default
 sh build_release.sh es default
 sh build_release.sh pt_BR default
 
-echo "ander theme"
-sh build_release.sh en_US ander
-sh build_release.sh es ander
-sh build_release.sh pt_BR ander
+# echo "ander theme"
+# sh build_release.sh en_US ander
+# sh build_release.sh es ander
+# sh build_release.sh pt_BR ander
 
 echo "done"
 cd ../
