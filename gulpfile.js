0c2a52dfab8395ae8ec957812c11c5e8161b5ff2
Cesar Augusto D. Azevedo <fxcesinha@FxOS.(none)>
2014-11-20 03:55:11 -0200

    fix gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,35 +5,28 @@ var gulp        = require('gulp'),
 
 var paths = {
         'sass'    : 'assets/themes/FX-Exchange/**/*.sass', // Sass files
-        'css'     : 'assets/themes/FX-Exchange/css/**.css',
-        'cssDir'  : 'assets/themes/FX-Exchange/css',
-        'cssDest' : '_site/assets/themes/FX-Exchange/css',
         'html'    : ['**/*.ejs']
     };
 
 gulp.task('harp-build', function(done){
     cp.exec('harp compile . _site', { stdio: 'inherit'}).on('close', done);
-    cp.exec('rm -r _site/.git');
-});
-
-gulp.task('harp-rebuild', ['harp-build'], function(){
-    browserSync.reload();
 });
 
 gulp.task('harp-sass', function(){
-	cp.exec('harp compile ' + paths.cssDir + ' ' + paths.cssDest, function(){
-		browserSync.reload("blinktrade.css", { stream: true});
-	});
+    browserSync.reload("blinktrade.css", { stream: true});
 });
 
-gulp.task("browser-sync", ['harp-rebuild' ], function() {
+gulp.task('harp-html', function(){
+	browserSync.reload('index.html', { stream: true });
+});
+
+gulp.task("browser-sync", function() {
     harp.server(__dirname, {
         port: 3000
     }, function() {
         browserSync({
-            server : {
-                baseDir: '_site'
-            }
+            proxy: 'localhost:3000',
+            open: true
         });
     });
 });
@@ -41,7 +34,9 @@ gulp.task("browser-sync", ['harp-rebuild' ], function() {
 //Watch your Sass, Less, Stylus files and will be compiled by HarpJS
 gulp.task('watch', function(){
     gulp.watch([paths.sass], ['harp-sass'] );
-    gulp.watch([paths.html, paths.css], ['harp-rebuild']);
+    gulp.watch([paths.html], ['harp-html']);
 });
 
 gulp.task('default', [ 'browser-sync', 'watch' ]);
+
+gulp.task('build', ['harp-build']);
